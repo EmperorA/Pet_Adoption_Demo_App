78974cd9e7c1d53dd3fc3ee25efc62ea88665393
Austine <austineuwumwonse@gmail.com>
2024-08-04 23:52:02 +0200

    error handing for db connection

--- server/server.js
+++ server/server.js
@@ -14,29 +14,40 @@ const dbConfig = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DATABASE_NAME,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false, // Adjust this based on your security needs
+  },
 });
 
 //Create http server
 const server = http.createServer(app);
 
 async function startServer() {
-  //here we will add await of DB connexion
-  dbConfig.connect(() => {
-    console.log("Connected to PostgreSQL database");
-  });
+  try {
+    // Connect to PostgreSQL
+    await dbConfig.connect(() => {
+      console.log("Connected to PostgreSQL database");
+    });
 
-  //TEST DB connexion: only for testing purposes
-  // dbConfig.query('SELECT * FROM test', (err, result) => {
-  //     if (err) {
-  //         console.error('Error executing query', err);
-  //     } else {
-  //         console.log('Query result:', result.rows);
-  //     }})
+    //TEST DB connexion: only for testing purposes
+    dbConfig.query("SELECT * FROM test", (err, result) => {
+      if (err) {
+        console.error("Error executing query", err);
+      } else {
+        console.log("Query result:", result.rows);
+      }
+    });
 
-  server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(
+      "Error starting the server or connecting to the database:",
+      error
+    );
+    process.exit(1); // Exit the process with failure code
+  }
 }
 
 startServer();
